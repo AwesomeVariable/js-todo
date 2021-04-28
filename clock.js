be45e22c513c47a1c27271521714aaf6dc611e08
AwesomeVariable <hyeonseo8312@gmail.com>
2021-04-28 16:01:18 +0900

    Create Clock

---
+++ clock.js
@@ -0,0 +1,15 @@
+const ClockContainer = document.querySelector(".js-clock")
+const ClockTitle = ClockContainer.querySelector("h1")
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  ClockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+init(); 
